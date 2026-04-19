f223eb0683f74301d15ad1a556c5e41f3b435032
diva934 <divaaa.agency@gmail.com>
2026-04-19 22:29:35 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 5501;
+const ROOT = __dirname;
+
+const mime = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+  '.woff2': 'font/woff2',
+};
+
+http.createServer((req, res) => {
+  let filePath = req.url === '/' ? '/index.html.html' : req.url;
+  filePath = path.join(ROOT, filePath);
+  const ext = path.extname(filePath);
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': mime[ext] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(PORT, () => console.log('BEEEF running on http://localhost:' + PORT));
