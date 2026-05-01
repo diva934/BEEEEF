a3492a1d1b719e4e17c8cae208b03a07eb31bb4b
pierrick <pierrickmanchec55@gmail.com>
2026-05-01 15:18:39 +0200

    fix: courbe graphique non-plate via enrichissement O-U deterministique frontend

--- _push.bat
+++ _push.bat
@@ -4,7 +4,7 @@ if exist .git\index.lock del /f /q .git\index.lock
 if exist .git\index del /f /q .git\index
 git reset
 git add -A
-git commit -m "feat: countdown timer + auto-settlement + historique synthetique realiste sur le graphique"
+git commit -m "fix: courbe graphique non-plate via enrichissement O-U deterministique frontend"
 git push origin main --force
 echo.
 git log --oneline -3
